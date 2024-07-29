eda17dbd9290d7183cf0666a2a624542cf2174d5
LtMilligan <gioele.miscia@gmail.com>
2024-07-29 18:31:55 +0200

    [UPD] Adding main content

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,10 +7,10 @@ export default {
     <header>
         <div class='container'>
             <div class='row'>
-                <div class='col-4'>
+                <div class='col-3'>
                     <img src="../../public/img/dc-logo.png" alt="logoDC">
                 </div>
-                <div class='col-8'>
+                <div class='col-9 align-self-center'>
                     <ul class="list-unstyled">
                         <li>
                             <a href="#">characters</a>
@@ -49,16 +49,31 @@ export default {
     </header>
 </template>
 <style lang="scss">
+@use '../styles/partials/variables';
+
+
+img {
+    margin: 10px 0;
+}
 
 ul {
     display: flex;
+    justify-content: flex-end;
 
     li {
-        padding: 20px 30px;
+        padding: 20px 15px;
 
         a {
             text-decoration: none;
             color: black;
+            font-weight: bold;
+            padding-bottom: 50px;
+            text-transform: uppercase;
+
+            &:hover {
+                color: variables.$primary-color;
+                border-bottom: 5px solid variables.$primary-color;
+            }
         }
     }
 }
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,11 +3,80 @@ export default {
     
 }
 </script>
-<template lang="">
-    <div>
-        
-    </div>
+<template>
+    <main>
+        <div class="bg-grey p-5">
+            <div class="container">
+                <div class="row content-grey">
+                    <div class="col-12 align-self-center">
+                        <p>--- Content goes here ---</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bg-primary p-5">
+            <div class="container">
+                <div class="row content-primary justify-content-evenly">
+                    <div class="col-2">
+                        <img src="../../public/img/buy-comics-digital-comics.png" alt="digital-comics">
+                        <p>DIGITAL COMICS</p>
+                    </div>
+                    <div class="col-2">
+                        <img src="../../public/img/buy-comics-merchandise.png" alt="merch">
+                        <p>DC MERCHANDISE</p>
+                    </div>
+                    <div class="col-2">
+                        <img src="../../public/img/buy-comics-subscriptions.png" alt="sub">
+                        <p>SUBSCRIPTION</p>
+                    </div>
+                    <div class="col-2">
+                        <img src="../../public/img/buy-comics-shop-locator.png" alt="locator">
+                        <p>COMIC SHOP LOCATOR</p>
+                    </div>
+                    <div class="col-2">
+                        <img src="../../public/img/buy-dc-power-visa.svg" alt="visa">
+                        <p>DC POWER VISA</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
-<style lang="">
-    
+<style lang="scss">
+@use '../styles/partials/variables';
+
+    .bg-grey {
+        background-color: rgb(39, 39, 39);
+    }
+    .bg-primary {
+        background-color: variables.$primary_color;
+    }
+    .content-grey {
+        color: white;
+    }
+    .content-primary {
+        color: white;
+    }
+    .col-12 {
+        p {
+            margin: 0;
+            font-size: 30px;
+        }
+    }
+
+    .col-2 {
+        align-self: center;
+
+        img {
+            max-width: 50px;
+            max-height: 50px;
+        }
+
+        p {
+            display: inline-block;
+            margin-left: 10px;
+            margin-bottom: 0;
+            font-size: 14px;
+        }
+    }
 </style>
