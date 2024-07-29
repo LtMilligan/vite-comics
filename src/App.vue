929a9570b44b85ea1115dfaee617517b9c348f4b
LtMilligan <gioele.miscia@gmail.com>
2024-07-29 16:50:49 +0200

    [UPD] Created logo and header menu

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
-<script setup>
-
+<script>
+    import AppHeader from './components/AppHeader.vue'
+    import AppMain from './components/AppMain.vue'
+    import AppFooter from './components/AppFooter.vue'
+    export default {
+        components: {
+            AppHeader,
+            AppMain,
+            AppFooter
+        }
+    }
 </script>
 
 <template>
-
+    <AppHeader />
+    <AppMain />
+    <AppFooter />
 </template>
 
-<style scoped>
-
+<style lang="scss">
+    @use './styles/generals.scss'
 </style>
