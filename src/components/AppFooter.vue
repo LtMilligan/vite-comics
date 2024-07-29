0089916e482a843b439943dc0f56c25fb739aa74
LtMilligan <gioele.miscia@gmail.com>
2024-07-29 22:14:41 +0200

    [UPD] Adding second footer section

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -54,33 +54,78 @@ export default {
                 </div>
             </div>
         </div>
+        <div class="bg-grey">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <button type="button" class="btn p-3 my-5">SIGN-UP NOW!</button>
+                    </div>
+                    <div class="col d-flex align-self-center justify-content-end">
+                        <h4>FOLLOW US</h4>
+                        <ul class="list-unstyled d-flex">
+                            <li><a href="#"><img src="../../public/img/footer-facebook.png" alt="facebook"></a></li>
+                            <li><a href="#"><img src="../../public/img/footer-twitter.png" alt="twitter"></a></li>
+                            <li><a href="#"><img src="../../public/img/footer-youtube.png" alt="youtube"></a></li>
+                            <li><a href="#"><img src="../../public/img/footer-pinterest.png" alt="pinterest"></a></li>
+                            <li><a href="#"><img src="../../public/img/footer-periscope.png" alt="periscope"></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     </footer>
 </template>
 <style lang="scss" scoped>
+@use '../styles/partials/variables';
+
+    .bg-grey {
+        background-color: rgb(45, 45, 45);
+        .container {
+            .row {
+                .col {
+                    .btn {
+                        border-radius: 0;
+                        border: 2px solid variables.$primary_color;
+                        color: white;
+                        font-weight: bold;
+                    }
+                    h4 {
+                        color: variables.$primary_color;
+                        font-weight: bold;
+                    }
+                    ul {
+                        li {
+                            margin-left: 20px;
+                        }
+                    }
+                }
+            }
+        }
+    }
     .jumbo {
         background-image: url('../../public/img/footer-bg.jpg');
         background-repeat: no-repeat;
         background-size: cover;
-    }
-    .row {
-        padding-top: 30px;
-        padding-bottom: 30px;
-        background-image: url('../../public/img/dc-logo-bg.png');
-        background-repeat: no-repeat;
-        background-position: right;
-        .col-2 {
-            h3 {
-                color: white;
-                margin: 20px 0;
-            }
-            ul{
-                li{
-                    a{
-                        text-decoration: none;
-                        color: gray;
-
-                        &:hover {
-                            color: white;
+        .row {
+            padding-top: 30px;
+            padding-bottom: 30px;
+            background-image: url('../../public/img/dc-logo-bg.png');
+            background-repeat: no-repeat;
+            background-position: right;
+            .col-2 {
+                h3 {
+                    color: white;
+                    margin: 20px 0;
+                }
+                ul{
+                    li{
+                        a{
+                            text-decoration: none;
+                            color: gray;
+    
+                            &:hover {
+                                color: white;
+                            }
                         }
                     }
                 }
